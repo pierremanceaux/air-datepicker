9520277a9927fa759ef4f1a209d6afee6a7e495e
Pierre Manceaux <pierremanceaux@users.noreply.github.com>
2016-03-13 13:09:46 +0100

    Create datepicker.fr.js

---
+++ src/js/i18n/datepicker.fr.js
@@ -0,0 +1,11 @@
+Datepicker.language['fr'] = {
+    days: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+    daysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    daysMin: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+    months: ['Janvier','Février','Mars','Avril','Mai','Juin', 'Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+    monthsShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jui', 'Jui', 'Aou', 'Sep', 'Oct', 'Nov', 'Déc'],
+    today: 'Aujourd\'hui',
+    clear: 'Réinitialiser',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 0
+};
